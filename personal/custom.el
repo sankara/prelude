654350b1fa6eb175dc496ffcf93fe5149cbef6df
Sankara Rameswaran <hello@sankara.me>
2019-10-14 21:58:47 -0700

    Whitespace limit

--- personal/custom.el
+++ personal/custom.el
@@ -5,11 +5,13 @@
  ;; If there is more than one, they won't work right.
  '(cider-auto-jump-to-error nil)
  '(cider-auto-select-error-buffer nil)
+ '(fill-column 120)
  '(package-selected-packages
    (quote
-    (rjsx-mode scss-mode dockerfile-mode yaml-mode neotree geiser json-mode js2-mode rainbow-mode elisp-slime-nav company counsel swiper ivy helm-projectile helm rainbow-delimiters clojure-mode markdown-mode paredit cider-spy cider-eval-sexp-fu cider-decompile cider exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree solarized-themes smartrep smartparens projectile operate-on-number move-text magit imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region editorconfig easy-kill discover-my-major diminish diff-hl crux browse-kill-ring beacon anzu ace-window)))
+    (scss-mode dockerfile-mode yaml-mode neotree geiser json-mode js2-mode rainbow-mode elisp-slime-nav company counsel swiper ivy helm-projectile helm rainbow-delimiters clojure-mode markdown-mode paredit cider-spy cider-eval-sexp-fu cider-decompile cider exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree solarized-themes smartrep smartparens projectile operate-on-number move-text magit imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region editorconfig easy-kill discover-my-major diminish diff-hl crux browse-kill-ring beacon anzu ace-window)))
  '(safe-local-variable-values (quote ((flycheck-disabled-checkers emacs-lisp-checkdoc))))
- '(smartparens-global-strict-mode nil))
+ '(smartparens-global-strict-mode nil)
+ '(whitespace-line-column nil))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
